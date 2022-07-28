7cd364b726eeaaaa1489874dc28ced8cc03b2983
Niharika186 <107666477+Niharika186@users.noreply.github.com>
2022-07-28 14:02:01 +0530

    Add files via upload

---
+++ Matplotlib.py
@@ -0,0 +1,57 @@
+# import matplotlib.pyplot as plt
+# x=[16,18,20,25,30,35]
+# y=[0,4,7,9,3,8]
+# plt.bar(x,y,color='green')
+# plt.show()
+
+# import numpy as np
+# p=np.array(22)
+# q=np.array([1,2,3])
+# r=np.array([[1,2,3],[3,2,1]])
+# s=np.array([[[1,2,3],[3,2,1]],[[4,5,6],[6,5,4]]])
+# print(p.ndim)
+# print(q.ndim)
+# print(r.ndim)
+# print(s.ndim)
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# m=np.array([1,4])
+# n=np.array([2,6])
+# plt.plot(m,n)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# m=np.array([0,4])
+# n=np.array([2,6])
+# plt.plot(m,n,'o')
+# plt.plot(m,n,'s')
+# plt.plot(m,n,'p')
+# plt.plot(m,n,'h')
+# plt.plot(m,n)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# m=np.array([0,4])
+# n=np.array([2,6])
+# plt.plot(m,n)
+# plt.plot(m,n,'p')
+# plt.plot(m,n,marker='p')
+# plt.plot(m,n,'s:k')
+# plt.plot(m,n,'s:c')
+# plt.plot(m,n,'s:y')
+# plt.plot(m,n,'s:b')
+# plt.plot(m,n,'s:w')
+# plt.plot(m,n,'s-b')
+# plt.plot(m,n,'s--r')
+# plt.plot(m,n,'s--g')
+# plt.plot(m,n,ls='dashed')
+# plt.plot(m,n,ls='dashdot')
+# plt.plot(m,n,ls='dotted')
+# plt.plot(m,n,ls='None')
+# plt.plot(m,n,ls='solid',lw='4')
+# plt.plot(m,n,ls='solid',lw='2')
+# plt.show()
+
